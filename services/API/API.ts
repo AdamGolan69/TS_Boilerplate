1977339438bb833ac660663ece6147f2e1fe0009
AdamGolan69 <slowmoon.adam@gmail.com>
2024-05-29 20:51:37 +0300

    Upgrade API class.

--- services/API/API.ts
+++ services/API/API.ts
@@ -2,32 +2,30 @@ export class API {
     constructor(private service: string = location.origin) { }
 
     GET(action: string) {
-        return this.baseRequest(action, {
-            method: 'GET'
-        });
+        return this.baseRequest(action, this.createInit('GET', ''));
     }
-    POST(action: string) {
-        return this.baseRequest(action, {
-            method: 'POST'
-        });
+    POST(action: string, payload: string) {
+        return this.baseRequest(action, this.createInit('POST', payload));
     }
-    PUT(action: string) {
-        return this.baseRequest(action, {
-            method: 'PUT'
-        });
+    PUT(action: string, payload: string) {
+        return this.baseRequest(action, this.createInit('PUT', payload));
     }
-    PATCH(action: string) {
-        return this.baseRequest(action, {
-            method: 'PATCH'
-        });
+    PATCH(action: string, payload: string) {
+        return this.baseRequest(action, this.createInit('PATCH', payload));
     }
-    DELETE(action: string) {
-        return this.baseRequest(action, {
-            method: 'DELETE'
-        });
+    DELETE(action: string, payload: string) {
+        return this.baseRequest(action, this.createInit('DELETE', payload));
     }
 
-    private baseRequest(action: string, init: RequestInit) {
-        return fetch(`${this.service}/${action}`, init);
+    private createInit(method: keyof API, payload: string): RequestInit {
+        return {
+            headers: { 'Content-Type': 'application/json' },
+            method,
+            body: method === 'GET' ? null : payload
+        }
+    }
+
+    private baseRequest(action: string, init: RequestInit): Promise<any> {
+        return fetch(`${this.service}/${action}`, init).then(res => res.json());
     }
 }
